da19ca72cd830593b8f2cf0144e230a3949b20c7
Mihajlo Vuckovic <mihajlo.vuckovic@vegait.rs>
2024-06-12 14:22:37 +0200

    Vrsenje updateReminder i sendEmail se vrsi preko threadpool-a

---
+++ src/main/java/com/demo/expense_tracker/thread_pool/AsyncConfig.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.demo.expense_tracker.thread_pool;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+/**
+ *
+ * @author mihajlo.vuckovic
+ */
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    
+    @Bean(name = "reminderExecutor")
+    public Executor reminderExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        executor.setThreadNamePrefix("ReminderThread-");
+        executor.initialize();
+        return executor;
+    }
+}
